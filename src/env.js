e5b2286a99268c3d45844447259ec84ed04e6e9f
Gallis <bussinessgallis@gmail.com>
2022-10-25 14:39:27 +0000

    add back

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const env = {
-    // baseServerURL: 'https://factureapp-production.up.railway.app',
-    baseServerURL: 'http://172.104.246.38:3100',
+    baseServerURL: 'https://factureapp-production.up.railway.app',
+    // baseServerURL: 'http://172.104.246.38:3100',
     tokenStorageName: '_auth__facture_app'
 }
